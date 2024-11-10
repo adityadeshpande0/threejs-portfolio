ecc7ab979f2a0c5ef7c951220a33d72b6179795f
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-11-10 12:43:57 +0530

    some assets added

---
+++ src/commons/commonData.js
@@ -0,0 +1,22 @@
+export const workExperiences = [
+  {
+    id: 1,
+    name: "Infosys",
+    pos: "Front End Developer",
+    duration: "2022 - Feb - Present",
+    title:
+      "Led end-to-end development of React components, from gathering requirements to deployment. Designed reusable components and interfaces with TypeScript, improving code maintainability and reducing development time. Optimized application performance using Redux and Redux Toolkit for efficient state management. Integrated APIs via React Query, enhancing data fetching and synchronization. Converted Figma wireframes into functional React components, improving UI/UX. Refactored code for better readability, performance, and scalability. Ensured high code quality with SonarQube-compliant, defect-free code.",
+    icon: "/assets/infosys-logo.jpg",
+    animation: "victory",
+  },
+  {
+    id: 2,
+    name: "Collabera",
+    pos: "Talent Specialist",
+    duration: "2021 - Mar - 2022 - Jan",
+    title:
+      "Worked as a US IT Recruiter manged entire life cycle of the recruitment process for clients in the US.",
+    icon: "/assets/collabera-logo.png",
+    animation: "clapping",
+  },
+];
