e949966418e537aab3c1bba880ce1d566f5b8e63
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-11-10 17:59:24 +0530

    header hero section developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Hero from "./sections/Hero";
+
 import Navbar from "./sections/Navbar";
 
 function App() {
   return (
-    <main className="max-w-7xl mx-auto relative">
-      <Navbar/>
+    <main className="max-w-5xl mx-auto relative">
+      <Navbar />
+      <Hero />
     </main>
   );
 }
