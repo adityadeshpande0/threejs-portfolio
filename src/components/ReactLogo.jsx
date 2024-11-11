f2d4da82451d78a582bc232652e4bce0e30eb8eb
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-11-11 23:55:49 +0530

    some changes

--- src/components/ReactLogo.jsx
+++ src/components/ReactLogo.jsx
@@ -5,11 +5,11 @@ const ReactLogo = (props) => {
 
   return (
     <Float floatIntensity={1}>
-      <group position={[8, 8, 0]} scale={0.3} {...props} dispose={null}>
+      <group position={[8, 8, 0]} scale={0.5} {...props} dispose={null}>
         <mesh
           geometry={nodes['React-Logo_Material002_0'].geometry}
           material={materials['Material.002']}
-          position={[0, 0.079, 0.181]}
+          position={[-6, -3, 0.181]}
           rotation={[0, 0, -Math.PI / 2]}
           scale={[0.392, 0.392, 0.527]}
         />
