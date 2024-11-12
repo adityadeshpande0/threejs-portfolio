139ef6afac2b650a6d98312aaa1b72a457f52891
adityadeshpande0 <adityadeshpande1@outlook.com>
2024-11-12 11:53:39 +0530

    alignment changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 const Button = ({ name, isBeam = false, containerClass, onClick }) => {
   return (
-    <div className={`flex justify-center w-full ${containerClass}`}>
+    <div className={`flex justify-center ${containerClass}`}>
       <button
-        class="flex items-center rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        className="flex items-center justify-center w-full rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
         type="button"
         onClick={onClick}
       >
